173d0838590b3bb4b7dccb7be54e63afb7ae5657
ChriTorr <christian.torli@itiszuccante.edu.it>
2024-03-20 13:31:21 +0100

    .

--- esercizioWait/src/Main.java
+++ esercizioWait/src/Main.java
@@ -2,14 +2,14 @@ public class Main{
     public static void main(String[] args) {
         ListHandler listHandler = new ListHandler(0);
 
-        int threadNumber = 100;
+        int threadNumber = 1000;
 
         ThreadRunnable[] t = new ThreadRunnable[threadNumber];
         Thread[] th = new Thread[threadNumber];
         Person[] people = new Person[threadNumber];
 
         for (int i = 0; i < threadNumber; i++) {
-            people[i] = new Person("gigi", "verde", "prova");
+            people[i] = new Person("gigi"+i, "verde"+i, "prova"+i);
             t[i] = new ThreadRunnable("th" + i, listHandler, i, people[i]);
             th[i] = new Thread(t[i]);
             th[i].start();    
